94b6fb7a39d6af0807571908944dc9195d99dfc6
Jack Pace <jpace2@harding.edu>
2022-05-11 09:10:33 -0500

    lesson2

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -20,9 +20,21 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-    System.out.println("This will output some text!");
-    System.out.println("This will output some text with a line break!");
-    System.out.println("This will also output some text with a line break!");
+    int numberOfWheelsOnRobot = 2;
+    double robotSpeedInchesPerSecond = 3.1;
+    
+    System.out.print("Number of wheels on the robot: ");
+    System.out.println(numberOfWheelsOnRobot);
+
+    System.out.print("The robot's current speed: ");
+    System.out.println(robotSpeedInchesPerSecond);
+
+    //The robot has stopped! Update the speed to 0 inches per second.
+    robotSpeedInchesPerSecond = 0;
+
+    System.out.print("The robot stopped! Now its new speed is: ");
+    System.out.println(robotSpeedInchesPerSecond);
+    
     RobotBase.startRobot(Robot::new);
   }
 }
