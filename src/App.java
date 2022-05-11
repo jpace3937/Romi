cd58e670e9dd67b52c1512a14bb3426dcc91bae2
Jack Pace <jpace2@harding.edu>
2022-05-11 18:08:14 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,9 +3,24 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter an integer:");
-        int userInput = input.nextInt();
-        System.out.println("User Input: " + userInput);
-        input.close();
+        int firstNumber = input.nextInt();
+        int secondNumber = input.nextInt();
+        int product = multiply(firstNumber, secondNumber);
+        System.out.println(product);
+    }
+    public static void welcome(){
+        System.out.println("Welcome to calculator!");
+    }
+    public static void add(int num1, int num2){
+        System.out.println(num1 + num2);
+    }
+    public static int multiply(int num1, int num2){
+        return num1*num2;
+    }
+    public static int subtract(int num1, int num2){
+        return num1 - num2;
+    }
+    public static int cube(int num){
+        return (int) Math.pow(num, 3);
     }
 }
