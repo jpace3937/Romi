c8dce2cda9f5c4e865f89c8a1a1e9e6c5c3b32ab
Jack Pace <jpace2@harding.edu>
2022-05-11 18:22:13 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,11 +1,26 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter an integer:");
-        int userInput = input.nextInt();
-        System.out.println("User Input: " + userInput);
-        input.close();
+        int dayOfWeek = 7;
+        if(dayOfWeek < 2){
+            System.out.println("Sunday");
+        }
+        else if(dayOfWeek < 3){
+            System.out.println("Monday");
+        }
+        else if(dayOfWeek < 4){
+            System.out.println("Tuesday");
+        }
+        else if(dayOfWeek < 5){
+            System.out.println("Wednesday");
+        }
+        else if(dayOfWeek < 6){
+            System.out.println("Thursday");
+        }
+        else if(dayOfWeek < 7){
+            System.out.println("Friday");
+        }
+        else{
+            System.out.println("Saturday");
+        }
     }
 }
