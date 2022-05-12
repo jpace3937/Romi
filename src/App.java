8e00d97a7c125ec34a5bdaecba86b1c0a7eef2a4
Jack Pace <jpace2@harding.edu>
2022-05-11 19:14:40 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,27 +1,62 @@
 import java.util.Scanner;
 
 public class App {
+    public static final Scanner input = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        int firstNumber = input.nextInt();
-        int secondNumber = input.nextInt();
-        int product = multiply(firstNumber, secondNumber);
-        System.out.println(product);
+        one();
+        two();
+        three();
+        four();
+        five();
+        six();
         input.close();
     }
-    public static void welcome(){
-        System.out.println("Welcome to calculator!");
+    public static void one(){
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
-    public static void add(int num1, int num2){
-        System.out.println(num1 + num2);
+    public static void two(){
+        int i = -10;
+        while(i < 21){
+            System.out.println(i);
+            i++;
+        }
     }
-    public static int multiply(int num1, int num2){
-        return num1*num2;
+    public static void three(){
+        double total = 0;
+        double max = input.nextDouble();
+        for(int i = 1; i <= max; i++){
+            total += (i*i);
+        }
+        System.out.println(total);
     }
-    public static int subtract(int num1, int num2){
-        return num1 - num2;
+    public static void four(){
+        double total = 0;
+        double max = input.nextDouble();
+        for(int i = 1; i <= max; i++){
+            total += (i);
+        }
+        System.out.println(total*total);
     }
-    public static int cube(int num){
-        return (int) Math.pow(num, 3);
+    public static void five(){
+        double total = 0;
+        double total1 = 0;
+        double max = input.nextDouble();
+        for(int i = 1; i <= max; i++){
+            total += (i);
+        }
+        for(int i = 1; i <= max; i++){
+            total1 += (i*i);
+        }
+        System.out.println((total*total) - total1);
+    }
+    public static void six(){
+        for(int i = 1; i < 11; i++){
+            for(int j = 1; j < 11; j++){
+                System.out.print(i*j + " ");
+            }
+            System.out.println();
+        }
     }
 }
